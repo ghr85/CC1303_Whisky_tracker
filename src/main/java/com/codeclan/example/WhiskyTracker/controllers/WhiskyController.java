0996d52ae6388d37db36d04b165e77a9d61f98b3
ghr85 <ghrenfrew@gmail.com>
2019-04-11 10:24:41 +0100

    corrected routing...doh!

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,10 +1,37 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/whiskies")
 public class WhiskyController {
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping
+    public List<Whisky> getAllWhiskies(){
+        return whiskyRepository.findAll();
+    }
+
+    @GetMapping(value="/year/{year}")
+    public List<Whisky> getWhiskiesByYear(int year){
+        return whiskyRepository.findWhiskiesByYear(year);
+    }
+    @GetMapping(value="/distillery/{distillery}/age/{age}")
+    public List<Whisky> getWhiskiesByYDistilleryAndAge(String distillery,int age){
+        return whiskyRepository.findWhiskyByDistilleryAndAge(distillery,age);
+    }
+    @GetMapping(value="/region/{region}")
+    public List<Whisky> getWhiskiesByREgion(String region){
+        return whiskyRepository.findWhiskiesByRegion(region);
+    }
+
 
 }
--- src/main/java/com/codeclan/example/WhiskyTracker/models/Distillery.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/models/Distillery.java
@@ -1,5 +1,7 @@
 package com.codeclan.example.WhiskyTracker.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +19,7 @@ public class Distillery {
 
     @Column(name = "region")
     private String region;
-
+    @JsonIgnoreProperties("distillery")
     @OneToMany(mappedBy = "distillery", fetch = FetchType.LAZY)
     private List<Whisky> whiskies;
 
